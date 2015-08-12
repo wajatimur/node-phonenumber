a76f85f2109061ba9b6879d237b5abc3562bb80e
Louy <me@l0uy.com>
2015-08-12 09:31:37 +0300

    More tests.

--- test/index.js
+++ test/index.js
@@ -5,8 +5,18 @@ var should = require('chai').should(),
 describe('#format', function(){
 
     it('parse MY phone number format', function(){
-        var phone_my = phone_util.parse('0139348815','MY');
-        phone_util.format(phone_my, phone.PhoneNumberFormat.INTERNATIONAL).should.equal('+60 13-934 8815');
+        var number = phone_util.parse('0139348815','MY');
+        phone_util.format(number, phone.PhoneNumberFormat.INTERNATIONAL).should.equal('+60 13-934 8815');
+    });
+
+    it('parse UK phone number format', function(){
+        var number = phone_util.parse('0845 313 66 66','GB');
+        phone_util.format(number, phone.PhoneNumberFormat.INTERNATIONAL).should.equal('+44 845 313 6666');
+    });
+
+    it('parse an international phone number', function(){
+        var number = phone_util.parse('0012422342353','GB');
+        phone_util.format(number, phone.PhoneNumberFormat.INTERNATIONAL).should.equal('+1 242-234-2353');
     });
 
 });
