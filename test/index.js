2adf523abdf23bd3878dd81898fb71dc0a9b129b
Louy <me@l0uy.com>
2015-08-12 09:41:35 +0300

    More tests.

--- test/index.js
+++ test/index.js
@@ -19,4 +19,31 @@ describe('#format', function(){
         phone_util.format(number, phone.PhoneNumberFormat.INTERNATIONAL).should.equal('+1 242-234-2353');
     });
 
+    it('validate number', function() {
+        var number;
+
+        number = phone_util.parse('23907','GB');
+        phone_util.isValidNumber(number).should.equal(false);
+
+        number = phone_util.parse('53453455','GB');
+        phone_util.isValidNumber(number).should.equal(false);
+
+        number = phone_util.parse('8453136666','GB');
+        phone_util.isValidNumber(number).should.equal(true);
+    });
+
+    it('format in RFC3966', function(){
+        var number = phone_util.parse('7239048346','TR');
+        phone_util.format(number, phone.PhoneNumberFormat.RFC3966).should.equal('tel:+90-7239048346');
+    });
+
+    it('format in E164', function(){
+        var number = phone_util.parse('5070000000','TR');
+        phone_util.format(number, phone.PhoneNumberFormat.E164).should.equal('+905070000000');
+    });
+
+    it('format to national', function(){
+        var number = phone_util.parse('+90 507 000 0000');
+        phone_util.format(number, phone.PhoneNumberFormat.NATIONAL).should.equal('0507 000 0000');
+    });
 });
